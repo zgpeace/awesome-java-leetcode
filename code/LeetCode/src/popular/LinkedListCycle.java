1ab5e14c469c24e1354b453640fde9dca4a7bb60
zgpeace <zgpeace@gmail.com>
2019-02-23 12:28:20 +0800

    LinkedListCycle

---
+++ code/LeetCode/src/popular/LinkedListCycle.java
@@ -0,0 +1,49 @@
+package popular;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+}
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        Set<Integer> pastSet = new HashSet<>();
+        while (head != null) {
+            if (pastSet.contains(head.val)) {
+                return true;
+            } else {
+                pastSet.add(head.val);
+                head = head.next;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasCycleWithTwoPoint(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slowNode = head;
+        ListNode fastNode = head.next;
+        while (slowNode != fastNode) {
+            if (slowNode == null || fastNode == null || fastNode.next == null) {
+                return false;
+            }
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;
+        }
+
+        return true;
+    }
+
+
+}

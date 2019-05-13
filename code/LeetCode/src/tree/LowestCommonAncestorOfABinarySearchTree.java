7e1d7db5eb23b299198330e758707a8c6bcf7d5f
zgpeace <zgpeace@gmail.com>
2019-05-13 16:06:11 +0800

    lowest common ancestor

---
+++ code/LeetCode/src/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,17 @@
+package tree;
+
+import common.TreeNode;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    if (root.val > p.val && root.val > q.val) {
+      return lowestCommonAncestor(root.left, p, q);
+    } else if (root.val < p.val && root.val < q.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    } else {
+      return root;
+    }
+  }
+}

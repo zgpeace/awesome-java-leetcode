b8c8c3731dbd6bc83d08ce55a7e6473ff6bd508d
zgpeace <zgpeace@gmail.com>
2019-05-24 12:16:01 +0800

    power of three

---
+++ code/LeetCode/src/number/PowerOfThree.java
@@ -0,0 +1,15 @@
+package number;
+
+// https://leetcode.com/problems/power-of-three/
+public class PowerOfThree {
+  public boolean isPowerOfThree(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    while (n % 3 == 0) {
+      n = n / 3;
+    }
+
+    return n == 1;
+  }
+}

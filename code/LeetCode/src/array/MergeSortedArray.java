2510f060c33c1182bf8945d0df06a7a04730a60a
zgpeace <zgpeace@gmail.com>
2019-07-16 10:04:01 +0800

    // nums2 is not empty, need to fill in; nums1 is content itself, no need to change

--- code/LeetCode/src/array/MergeSortedArray.java
+++ code/LeetCode/src/array/MergeSortedArray.java
@@ -6,11 +6,13 @@ public class MergeSortedArray {
     int index = n + m - 1;
     int oneIndex = m - 1;
     int twoIndex = n - 1;
+    // big --> small, descending
     while (oneIndex >= 0 && twoIndex >= 0) {
       nums1[index--] = nums1[oneIndex] > nums2[twoIndex] ? nums1[oneIndex--] : nums2[twoIndex--];
     }
-    while (index >= 0) {
-      nums1[index--] = oneIndex >= 0 ? nums1[oneIndex--] : nums2[twoIndex--];
+    // nums2 is not empty, need to fill in; nums1 is content itself, no need to change
+    while (twoIndex >= 0) {
+      nums1[index--] = nums2[twoIndex--];
     }
   }
 

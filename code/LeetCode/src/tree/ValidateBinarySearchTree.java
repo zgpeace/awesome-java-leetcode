a2b16edc7efb4bd07850cdec04e1e8dd151f8569
zgpeace <zgpeace@gmail.com>
2019-07-16 09:59:47 +0800

    validate binary search tree with iterate

--- code/LeetCode/src/tree/ValidateBinarySearchTree.java
+++ code/LeetCode/src/tree/ValidateBinarySearchTree.java
@@ -2,6 +2,9 @@ package tree;
 
 import common.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // https://leetcode.com/problems/validate-binary-search-tree/
 public class ValidateBinarySearchTree {
 
@@ -26,5 +29,46 @@ public class ValidateBinarySearchTree {
 
 
     return isValidBSTWithLowerAndUpper(root.left, lower, root.val) && isValidBSTWithLowerAndUpper(root.right, root.val, upper);
+  }
+
+  public boolean isValidBSTWithIterate(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    Queue<Integer> valleyQueue = new LinkedList<>();
+    valleyQueue.add(null);
+    Queue<Integer> peakQueue = new LinkedList<>();
+    peakQueue.add(null);
+    TreeNode current;
+    Integer valley;
+    Integer peak;
+    while (!queue.isEmpty()) {
+      current = queue.poll();
+      valley = valleyQueue.poll();
+      peak = peakQueue.poll();
+      if (valley != null && current.val <= valley) {
+        return false;
+      }
+      if (peak != null && current.val >= peak) {
+        return false;
+      }
+
+      if (current.left != null) {
+        queue.add(current.left);
+        valleyQueue.add(valley);
+        peakQueue.add(current.val);
+      }
+
+      if (current.right != null) {
+        queue.add(current.right);
+        valleyQueue.add(current.val);
+        peakQueue.add(peak);
+      }
+
+    }
+
+    return true;
   }
 }

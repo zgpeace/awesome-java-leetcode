433a8ffcee91d84333ae4175087492a7f4c709c0
zgpeace <zgpeace@gmail.com>
2020-01-13 12:45:02 +0800

    Restore IP Address, iterate solution

--- code/LeetCode/src/backtracking/RestoreIPAddress.java
+++ code/LeetCode/src/backtracking/RestoreIPAddress.java
@@ -10,7 +10,8 @@ public class RestoreIPAddress {
   public static void main(String[] args) {
     RestoreIPAddress obj = new RestoreIPAddress();
     String s = "25525511135";
-    List<String> resultList = obj.restoreIpAddresses(s);
+    //List<String> resultList = obj.restoreIpAddresses(s);
+    List<String> resultList = obj.restoreIpAddressesWithIterate(s);
     System.out.println(Arrays.toString(resultList.toArray()));
   }
 
@@ -32,7 +33,7 @@ public class RestoreIPAddress {
         resultList.add(stored);
     }
 
-    if (start == s.length() || count == 4) {
+    if (start >= s.length() || count == 4) {
       return;
     }
 
@@ -46,5 +47,44 @@ public class RestoreIPAddress {
       }
       dfs(resultList, s, start + i, stored + part + (count == 3 ? "" : "."), count + 1);
     }
+  }
+
+
+  public List<String> restoreIpAddressesWithIterate(String s) {
+    List<String> resultList = new ArrayList<String>();
+    if (s == null || s.length() < 4) {
+      return resultList;
+    }
+
+    int len = s.length();
+    String splitS = ".";
+    // iterate
+    for (int fisrt = 1; fisrt < 4 && fisrt < len - 2; fisrt++) {
+      for (int second = fisrt + 1; second < fisrt + 4 && second < len - 1; second++) {
+        for (int third = second + 1; third < second + 4 && third < len; third++) {
+          String part1 = s.substring(0, fisrt);
+          String part2 = s.substring(fisrt, second);
+          String part3 = s.substring(second, third);
+          String part4 = s.substring(third);
+          if (valideIP(part1) && valideIP(part2) && valideIP(part3) && valideIP(part4)) {
+            String result = part1 + splitS + part2 + splitS + part3 + splitS + part4;
+            resultList.add(result);
+          }
+        }
+      }
+
+    }
+
+    return resultList;
+  }
+
+  private Boolean valideIP(String part) {
+    if (part.length() == 0 || part.length() > 3
+        || (part.startsWith("0") && part.length() != 1)
+        || (part.length() == 3 && Integer.valueOf(part) > 255 )) {
+      return false;
+    }
+
+    return true;
   }
 }

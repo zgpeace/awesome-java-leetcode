3ac5ff24eb5bcfd2857be1e72b5cd5cd838d6105
zgpeace <zgpeace@gmail.com>
2020-01-19 08:41:44 +0800

    dp solution for palindrome partitioning

--- code/LeetCode/src/backtracking/PalindromePartitioning.java
+++ code/LeetCode/src/backtracking/PalindromePartitioning.java
@@ -1,8 +1,6 @@
 package backtracking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 // https://leetcode.com/problems/palindrome-partitioning/
 public class PalindromePartitioning {
@@ -10,7 +8,8 @@ public class PalindromePartitioning {
   public static void main(String[] args) {
     String s = "aab";
     PalindromePartitioning obj = new PalindromePartitioning();
-    List<List<String>> resultList = obj.partition(s);
+    //List<List<String>> resultList = obj.partition(s);
+    List<List<String>> resultList = obj.partitionWithDP(s);
     System.out.println(Arrays.toString(resultList.toArray()));
   }
 
@@ -52,5 +51,31 @@ public class PalindromePartitioning {
     }
 
     return true;
+  }
+
+  public List<List<String>> partitionWithDP(String s) {
+    int len = s.length();
+    List<List<String>>[] results = new List[len + 1];
+    results[0] = new ArrayList<List<String>>();
+    results[0].add(new ArrayList<String>());
+
+    boolean[][] pair = new boolean[len][len];
+    for (int i = 0; i < len; i++) {
+      results[i + 1] = new ArrayList<List<String>>();
+      for (int left = 0; left <= i; left++) {
+        if (s.charAt(left) == s.charAt(i) && (i-left <= 1 || pair[left + 1][i - 1])) {
+          pair[left][i] = true;
+          String sub = s.substring(left, i + 1);
+          for (List<String> list: results[left]) {
+            List<String> newList = new ArrayList<String>(list);
+            newList.add(sub);
+            results[i + 1].add(newList);
+          }
+        }
+      }
+
+    }
+
+    return results[len];
   }
 }
